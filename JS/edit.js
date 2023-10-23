68f096256a71c3e9e81e9258febec01f6d702343
Ахмедов Руфат Фаиг <Ahmed_ur54@itstep.edu.az>
2023-10-23 20:49:04 +0400

    Add Edit.js

---
+++ JS/edit.js
@@ -0,0 +1,27 @@
+import Task from "../Models/task.js";
+import tasks from "../globals.js";
+
+const goHomeButton = document.getElementById("go-home-btn");
+
+goHomeButton.addEventListener("click", e => {
+    window.location.href = '/index.html';
+})
+
+const myTask = Task.getObjectToTask(myObjects.find(task => task.id === myParams.get('id')));
+const myObjects = JSON.parse(localStorage.getItem("tasks"));
+
+const Form = document.getElementById("form");
+const myParams = new URLSearchParams(window.location.search);
+const Name = document.getElementById('name');
+const Description = document.getElementById('description');
+const Status = document.getElementById('status');
+
+Form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    myTask.setName = Name.value; 
+      myTask.setDescription = Description.value; 
+      myTask.setIsCompleted = Status.checked;
+    console.log(Status.checked);
+
+    tasks.editTask(myTask.getId, myTask);
+});
